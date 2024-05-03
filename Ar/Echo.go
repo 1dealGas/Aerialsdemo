0817e77169ffccf777bd09c5fdc9407d2ef01706
Fantx Fux <109365408+Fuxfantx@users.noreply.github.com>
2024-05-03 15:50:32 +0800

    Add Echo GO & Arf3 Scripts

---
+++ Ar/Echo.go
@@ -0,0 +1,71 @@
+embedded_components {
+  id: "Helper"
+  type: "sprite"
+  data: "default_animation: \"Icon\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_SCREEN\n"
+  "size {\n"
+  "  x: 37.0\n"
+  "  y: 37.0\n"
+  "  z: 0.0\n"
+  "  w: 0.0\n"
+  "}\n"
+  "size_mode: SIZE_MODE_MANUAL\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/Atlas/UI.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.06
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "Sprite"
+  type: "sprite"
+  data: "default_animation: \"Hint\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "slice9 {\n"
+  "  x: 15.0\n"
+  "  y: 15.0\n"
+  "  z: 15.0\n"
+  "  w: 15.0\n"
+  "}\n"
+  "size {\n"
+  "  x: 80.0\n"
+  "  y: 80.0\n"
+  "  z: 0.0\n"
+  "  w: 0.0\n"
+  "}\n"
+  "size_mode: SIZE_MODE_MANUAL\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/Atlas/UI.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.38268343
+    w: 0.9238795
+  }
+  scale {
+    x: 0.88
+    y: 0.88
+    z: 1.0
+  }
+}
